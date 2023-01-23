1f12736a317b2cfa657ba0792571159ca1dde752
Jason Kadman <jkadman231@gmail.com>
2023-01-23 06:53:10 +0000

    databasejs connects to sql lightbnb database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,3 +1,14 @@
+const { Pool } = require('pg');
+
+const pool = new Pool ({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+})
+// the following assumes that you named your connection variable `pool`
+pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
